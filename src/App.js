cc65fcc3727d8db7165634e60206e609a5709f09
Keith Tomlinson <kbarretttomlinson@gmail.com>
2021-05-25 19:39:50 -0500

    creates user component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Counter from './counter/Counter.jsx';
 import CounterDisplay from './counterDisplay/CounterDisplay.jsx';
+import User from './user/User.jsx';
 
 const counters = ["Dale", "Laura", "Bob", "Mike"];
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <User></User>
         <CounterDisplay></CounterDisplay>
         {counters.map((counter => {
           return(<Counter key = {counter} name = {counter}></Counter>);
---
+++ src/user/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const User = () => {
+    return (
+        <React.Fragment>
+            <h1>User Component</h1>
+        </React.Fragment>
+    );
+}
+
+export default User;
